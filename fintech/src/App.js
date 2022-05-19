1763ee5ba2b1d049a5b557e072c745e80e52c85c
Minah <51119413+yucosmosme@users.noreply.github.com>
2022-05-19 19:19:28 +0900

    -

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,9 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from './components/Welcome';
-import ArrayMap from './components/ArrayMap';
+// import Welcome from './components/Welcome';
+// import ArrayMap from './components/ArrayMap';
 import AxiosTest from './pages/AxiosTest';
+import NewsApiPage from './pages/NewsApiPage';
 // import StateAndEvent from './components/StateAndEvent';
 // import AppBar from './components/common/AppBar';
 
@@ -11,8 +12,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/a" element={<Welcome/>}/>
-        <Route path="/b" element={<ArrayMap/>}/>
+        <Route path="/news" element={<NewsApiPage/>}/>
+        {/* <Route path="/b" element={<ArrayMap/>}/> */}
         <Route path="/axiosTest" element={<AxiosTest/>}/>
       </Routes>
     </BrowserRouter>
--- fintech/src/pages/NewsApiPage.js
+++ fintech/src/pages/NewsApiPage.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppBar from '../components/common/AppBar'
+import SearchInput from '../components/news/SearchInput'
+import SearchResult from '../components/news/SearchResult'
 
 const NewsApiPage = () => {
+
+  const [searchInput, setSearchInput] = useState('')
+  const [searchList, setSearchList] = useState([])
+
+  const searchInputHandleChange = () => {
+    //검색어 변경
+  }
+
+  const searchButtonHandleClick = () => {
+    //검색버튼 클릭
+  }
+
   return (
     <>
       <AppBar title={"뉴스검색"}/>
       {/* 검색어 입력 컴포넌트 */}
+      <SearchInput handleChange={searchInputHandleChange} handleClick={searchButtonHandleClick}/>
       {/* 검색 결과 출력 컴포넌트 */}
+      <SearchResult/>
     </>
   )
 }
